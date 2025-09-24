1ddeccd9edd16b41539611518c5489e16bdd60f2
Sam Gardner <samuel.gardner@ttu.edu>
2025-09-24 14:53:11 -0500

    scanning, date filtering

--- archive_sort_cleanup.py
+++ archive_sort_cleanup.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
+# Sorts TTU Slow Antenna network data and prunes unininteresting data by LMA
+# Created 14 September 2025 by Sam Gardner <samuel.gardner@ttu.edu> 
 
 import argparse
-from os import path, walk, listdir, remove, rmdir
+from os import path
+from glob import glob
+from datetime import datetime as dt
+import numpy as np
 
 parser = argparse.ArgumentParser(description='Slow antenna archive pruning and sorting.')
-
 parser.add_argument('--archive-root', '-r', type=str, required=True, default='/mnt/reservoir/SA_DATA/', help='Top level directory of slow antenna data archive.')
 parser.add_argument('--lma-data', '-l', type=str, default=None, help='Directory containing LMA data for correlation with slow antenna data. Leave unspecified to skip pruning.')
 parser.add_argument('--dry-run', '-n', action='store_true', help='Log actions without making any changes.')
@@ -15,19 +19,127 @@ if __name__ == '__main__':
     archive_root = args.archive_root
     dry_run = args.dry_run
 
-    for dirpath, dirnames, filenames in walk(archive_root):
-        print(f'Processing directory: {dirpath}')
-        print('=======================')
-
-        # TODO: Check for dates misaligned with parent directory and move to correct dir
-        # TODO: Check for directories that aren't dates and arent sensor_XX
-        # TODO: check for directories containing multiple CPU IDs
-        # TODO: find same CPU ID in different sensor directories at same time
-        # TODO: Create a log of CPU IDs and sensor numbers to move sensor IDs to the correct locations
-        # TODO: Find dates of old deployments and associate lat/lon and maybe even CPU ID to convert to new file format
-        # TODO: find files with [this issue](https://github.com/wx4stg/Bruning_Slow_Antenna_Software/issues/3) and try to recover the missing locations?
-        # TODO: Do the same for files with NO_FIX
-        # TODO: find 0-byte files and remove them
-        # TODO: plot time differences between files to find gaps and misplaced files
-        # TODO: incorporate LMA based pruning of unininteresting data
-        # TODO: remove empty directories
+    if not dry_run:
+        print('----> FILESYSTEM CHANGES CAN BE PERFORMED <----')
+        from os import remove, rmdir, rename
+        from time import sleep
+        print('----> Press CTRL + C in the next 5 seconds to abort! <----')
+        sleep(5)
+    date_directories = sorted(glob(path.join(archive_root, "*")))
+    dates_to_proc = []
+    for date_dir in date_directories:
+        try:
+            this_date = dt.strptime(path.basename(date_dir), '%Y%m%d')
+        except ValueError as e:
+            if 'does not match format' in str(e):
+                if dry_run:
+                    print(f'Would remove {date_dir} (first level directory does not conform to date format string YYYYMMDD/)')
+                else:
+                    print(f'Removing {date_dir}')
+                    rmdir(date_dir)
+        sensor_dirs = sorted(glob(path.join(date_dir, '*')))
+        for sensor_dir in sensor_dirs:
+            if path.basename(sensor_dir).startswith('sensor_') and len(path.basename(sensor_dir)) == 9:
+                try:
+                    sensor_num = int(sensor_dir[-2:])
+                except Exception as e:
+                    print(str(e))
+                    print('-----')
+                    if dry_run:
+                        print(f'Would remove {sensor_dir} (second level directory does not conform to format sensor_XX)')
+                    else:
+                        print(f'Removing {sensor_dir}')
+                        rmdir(sensor_dir)
+            else:
+                if dry_run:
+                    print(f'Would remove {sensor_dir} (second level directory does not conform to format sensor_XX)')
+                else:
+                    print(f'Removing {sensor_dir}')
+                    rmdir(sensor_dir)
+            if str(sensor_num).zfill(2) != sensor_dir[-2:]:
+                raise ValueError('Directory {sensor_dir} does not conform to format sensor_XX, but I\'m not sure what to do with it')
+            sensor_dir_content = sorted(glob(path.join(sensor_dir, '*')))
+            sensor_dir_content_names = [path.basename(s) for s in sensor_dir_content]
+            # -1 = invalid, 0 = SA_log.out, 1 = time only (old old), 2 = time, relay (old), 3 = time, location, CPU, GPS err, relay (current)
+            filename_spec = np.zeros(len(sensor_dir_content))
+            for i, psbl_rawfile in enumerate(sensor_dir_content_names):
+                if psbl_rawfile == 'SA_log.out':
+                    filename_spec[i] = 0
+                    continue
+                if not (psbl_rawfile.endswith('.raw') & ('_' in psbl_rawfile)):
+                    filename_spec[i] = -1
+                    if dry_run:
+                        print(f'Would remove {sensor_dir_content[i]} (content of sensor dir is not a raw data file or SA_log.out)')
+                    else:
+                        print(f'Removing {sensor_dir_content[i]}')
+                        remove(sensor_dir_content[i])
+                    continue                    
+                rawfile_split = psbl_rawfile.replace('.raw', '').split('_')
+                if len(rawfile_split) == 2:
+                    # this is an 'old old' file type
+                    filename_spec[i] = 1
+                    this_file_dt = dt.strptime(psbl_rawfile, '%Y%m%d%H%M%S_%f.raw')
+                    this_file_relay = None # TODO: figure this out from the SA_log.out or cronlog.txt or cronlog.out or data_collect.py, if available
+                    # TODO: maybe read this info from some sort of user-provided csv and convert to a "2" or "3" spec filename automatically
+                    this_file_lat = None
+                    this_file_lon = None
+                    this_file_alt = None
+                    this_file_gps_err = None
+                    this_file_cpu = None
+                elif len(rawfile_split) == 3:
+                    # this is an 'old' file type
+                    filename_spec[i] = 2
+                    this_file_dt = dt.strptime(rawfile_split[0]+rawfile_split[1], '%Y%m%d%H%M%S%f')
+                    this_file_lat = None
+                    this_file_lon = None
+                    this_file_alt = None
+                    this_file_gps_err = None
+                    this_file_cpu = None
+                    this_file_relay = rawfile_split[2]
+                elif len(rawfile_split) == 9:
+                    # this is a current filename
+                    filename_spec[i] = 3
+                    this_file_dt = dt.strptime(rawfile_split[0]+rawfile_split[1]+rawfile_split[2], '%Y%m%d%H%M%S%f')
+                    this_file_lat = None if rawfile_split[3] == 'NO' else float(rawfile_split[3])
+                    this_file_lon = None if rawfile_split[4] == 'FIX' else float(rawfile_split[4])
+                    this_file_alt = None if rawfile_split[5] == '2Donly' else float(rawfile_split[5])
+                    this_file_gps_err = float(rawfile_split[6])
+                    this_file_cpu = int(rawfile_split[7], 16)
+                    this_file_relay = rawfile_split[8]
+                elif len(rawfile_split) == 6:
+                    # This is a current file with no GPS... see https://github.com/wx4stg/Bruning_Slow_Antenna_Software/issues/3    ['20240815', '102012', '875419', '0.00', '93af7e18', 'a'
+                    filename_spec[i] = 3
+                    this_file_dt = dt.strptime(rawfile_split[0]+rawfile_split[1]+rawfile_split[2], '%Y%m%d%H%M%S%f')
+                    this_file_lat = None
+                    this_file_lon = None
+                    this_file_alt = None
+                    this_file_gps_err = float(rawfile_split[3])
+                    this_file_cpu = int(rawfile_split[4], 16)
+                    this_file_relay = rawfile_split[5]
+                    new_file_name = f'{this_file_dt.strftime("%Y%m%d_%H%M%S_%f")}_NO_FIX_2Donly_{this_file_gps_err}_{this_file_cpu:x}_{this_file_relay}.raw'
+                    new_file_path = sensor_dir_content[i].replace(psbl_rawfile, new_file_name)
+                    if dry_run:
+                        print(f'Would move {sensor_dir_content[i]} to {new_file_path} (Fix for https://github.com/wx4stg/Bruning_Slow_Antenna_Software/issues/3)')
+                    else:
+                        print(f'Moving {sensor_dir_content[i]} to {new_file_path}')
+                        rename(sensor_dir_content[i], new_file_path)
+                # Detect misplaced rawfile dates
+                if this_file_dt.replace(hour=0, minute=0, second=0, microsecond=0) != this_date.replace(hour=0,	minute=0, second=0, microsecond=0):
+                    new_file_path = path.join(archive_root, this_file_dt.strftime('%Y%m%d'), path.basename(sensor_dir), psbl_rawfile)
+                    if dry_run:
+                        print(f'Would move {sensor_dir_content[i]} to {new_file_path} (file has different date than parent directory)')
+                    else:
+                        print(f'Moving {sensor_dir_content[i]} to {new_file_path}')
+                        rename(sensor_dir_content[i], new_file_path)
+                # Detect multiple CPU IDs in same directory
+                
+    # TODO: check for directories containing multiple CPU IDs
+    # TODO: find same CPU ID in different sensor directories at same time
+    # TODO: Create a log of CPU IDs and sensor numbers to move sensor IDs to the correct locations
+    # TODO: Find dates of old deployments and associate lat/lon and maybe even CPU ID to convert to new file format
+    # TODO: find files with [this issue](https://github.com/wx4stg/Bruning_Slow_Antenna_Software/issues/3) and try to recover the missing locations?... kinda done, renamed to NO_FIX to be handled by below
+    # TODO: Do the same for files with NO_FIX
+    # TODO: find 0-byte files and remove them
+    # TODO: plot time differences between files to find gaps and misplaced files
+    # TODO: incorporate LMA based pruning of unininteresting data
+    # TODO: remove empty directories
